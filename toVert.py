b03c058c8a40e67757e8834315778f1838ff6fe1
Matthew Ireland <matthew@ireland.cx>
2024-05-09 22:26:39 +0100

    Don't skip lines if the morphology field is missing
    Also warn the user if a line is missing, and tell the user how many lines  (if any) were missing morphology, and print the total number of lines processed.

--- toVert.py
+++ toVert.py
@@ -10,6 +10,9 @@ with open(conllu_file_path, 'r') as conllu_file:
     conllu_str_lines = conllu_file.read().splitlines()
 
     doc_id = Path(conllu_file_path).stem
+    lines_skipped = 0
+    lines_missing_morph = 0
+    total_lines = 0
 
     with open(f"{doc_id}.vert", "w") as vert_file:
         inSentence = False
@@ -29,14 +32,21 @@ with open(conllu_file_path, 'r') as conllu_file:
 
             fields = line.split()
 
-            if len(fields) < 6:
+            if len(fields) < 3:
                 # malformed line (e.g. empty line at end of doc)
+                lines_skipped = lines_skipped + 1
                 continue
 
             token = fields[1]
             lemma = fields[2]
             pos = fields[3]
-            morphology = fields[5]
+            if len(fields) >= 6:
+                morphology = fields[5]
+            else:
+                morphology = ""
+                lines_missing_morph = lines_missing_morph + 1
+
+            total_lines = total_lines + 1
 
             if not inSentence:
                 vert_file.write("<s>\n")
@@ -51,4 +61,14 @@ with open(conllu_file_path, 'r') as conllu_file:
         vert_file.write("</p>\n")
         vert_file.write("</doc>\n")
 
+print(f"Total lines: {total_lines}")
 
+if lines_missing_morph > 0:
+    print(f"WARNING: {lines_missing_morph} are missing morphology")
+        
+if lines_skipped == 1:
+    print("WARNING: skipped 1 line (maybe you had a blank line at the end of the input file?)")
+        
+if lines_skipped > 1:
+    print(f"WARNING: skipped {lines_skipped} lines")
+        
